274048f5676ebc946bc765b1ea6b328722d47d49
sreejith87 <sreejith.darmatek@gmail.com>
2018-06-25 18:54:18 +0800

    first commit

---
+++ src/android/SimpleCalculator.java
@@ -0,0 +1,54 @@
+package cordova_plugin_simplecalculator;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class SimpleCalculator extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // if (action.equals("coolMethod")) {
+        //     String message = args.getString(0);
+        //     this.coolMethod(message, callbackContext);
+        //     return true;
+        // }
+        if (action.equals("addMethod")) {
+            this.addMethod(args, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void addMethod(JSONArray args, CallbackContext callback) {
+        if(args != null){
+            try
+            {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success(" " + (p1+p2) );
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+        else
+        {
+            callback.error("Dont pass null values");
+        }
+    }
+    // private void coolMethod(String message, CallbackContext callbackContext) {
+    //     if (message != null && message.length() > 0) {
+    //         callbackContext.success(message);
+    //     } else {
+    //         callbackContext.error("Expected one non-empty string argument.");
+    //     }
+    // }
+}
