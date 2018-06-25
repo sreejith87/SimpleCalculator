b1f9b483340562288aef959995ce003824d26b29
sreejith87 <sreejith.darmatek@gmail.com>
2018-06-25 20:43:59 +0800

    fourth

--- www/SimpleCalculator.js
+++ www/SimpleCalculator.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.addMethod = function (arg0, success, error) {
-    exec(success, error, 'SimpleCalculator', 'addMethod', arg0);
+    exec(success, error, 'SimpleCalculator', 'addMethod', [arg0]);
 };
 
 // exports.coolMethod = function (arg0, success, error) {
